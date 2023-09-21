1c0ddf3e2255273e1dc5fc6b33292f5f64db7238
cynthiacodes <cjnkstudy01@hotmail.com>
2023-09-21 10:36:24 +0100

    tesing time performance for twoSumV2

--- src/twoSumV2.ts
+++ src/twoSumV2.ts
@@ -1,6 +1,8 @@
 // for each num of the array
 //     array.indexOf(target - num)
 
+import { generateWorstCaseArray } from "./generateWorstCaseArray";
+
 function twoSumV2(numArray: number[], targetSum: number) {
     let indicesArray: number[] = [];
     for (let i = 0; i < numArray.length; i++) {
@@ -14,14 +16,28 @@ function twoSumV2(numArray: number[], targetSum: number) {
 }
 
 // test
-console.log(
-    "function twoSumV2 ",
-    twoSumV2([2, 7, 11, 15], 9),
-    "should return [0,1]"
-);
-console.log(
-    "function twoSumV2 ",
-    twoSumV2([3, 2, 4], 6),
-    "should return [1,2]"
-);
-console.log("function twoSumV2 ", twoSumV2([3, 3], 6), "should return [0,1]");
+// console.log(
+//     "function twoSumV2 ",
+//     twoSumV2([2, 7, 11, 15], 9),
+//     "should return [0,1]"
+// );
+// console.log(
+//     "function twoSumV2 ",
+//     twoSumV2([3, 2, 4], 6),
+//     "should return [1,2]"
+// );
+// console.log("function twoSumV2 ", twoSumV2([3, 3], 6), "should return [0,1]");
+
+let n = 10;
+for (let i = 0; i < 10; i++) {
+    const { arr, targetSum } = generateWorstCaseArray(n);
+
+    const startTime = performance.now();
+    twoSumV2(arr, targetSum);
+    const stopTime = performance.now();
+    const elapsedTime = stopTime - startTime;
+
+    console.log(`${n} elements,${elapsedTime}`);
+
+    n *= 10;
+}
